b308cef2fc6b5c70c69fa10ae7f964f942ec0beb
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-17 23:50:12 -0300

    Mais um dia vencido hehehehehehehehehe

--- operadores/11_logicos_1.js
+++ operadores/11_logicos_1.js
@@ -1,5 +1,20 @@
-let temDinheiro = true;
-let estaEnsolarado = true;
+let temDinheiro = false;
+let estaEnsolarado = false;
+let carroEstaNaGaragem = true;
 
-let resultadoE = "Vai para o shopping?";
-let resultadoXor = "vai comprar a tv32?"
+let resultadoE = "#1- (AND) Você irá ao shopping? "
+resultadoE += temDinheiro && estaEnsolarado;
+console.log(resultadoE);
+
+let resultadoOU = "#2- (OR) Você irá ao shopping? "
+resultadoOU += estaEnsolarado || carroEstaNaGaragem;
+console.log(resultadoOU);
+
+console.log(true ^ false);
+console.log(true !== true);
+console.log(true !== false);
+console.log(false !== true);
+console.log(false !== false);
+
+console.log("Não verdadeiro: " + !true);
+console.log("Não falso: " + !false);
