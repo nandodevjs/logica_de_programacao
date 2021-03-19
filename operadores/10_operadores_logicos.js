41a03687d002b0b697f9be99af6abce444b19506
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-18 22:54:22 -0300

    Unários

---
+++ operadores/10_operadores_logicos.js
@@ -0,0 +1,7 @@
+// Tipo And: Todas as afirmações necessitam ser true para o resultado ser true
+
+// OR: Se alguma reposta ser true, o resultado é true.
+
+// Xor: True + True = False / False + False = False
+
+// NOT: Inverter com !
---
+++ operadores/14_unarios_1.js
@@ -0,0 +1,10 @@
+let a = 1;
+
+a = a + 1; //infix: Operador operando entre dois operandos.
+a += 1;
+a++; //Postfix: Opera depois do operando.
+++a; //Prefix: Opera antes do operando.
+a--;
+--a;
+
+console.log(a);
