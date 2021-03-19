db35a43c302967e4c072c2ed04017dd692d3e9c1
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-18 22:07:43 -0300

    Desafio Aritméticos Relacionais Lógicos

---
+++ operadores/13_desafio_logico.js
@@ -0,0 +1,15 @@
+// Duas expressões para resultar em TRUE (Aritméticos/Relacionais/Lógicos)
+
+let verdadeira1 = 1 + 1 <= 2 || 3;
+let verdadeira2 = 2 - 1 >= 1 !== 3;
+
+console.log(verdadeira1);
+console.log(verdadeira2);
+
+// Duas expressões para resultar em False (Aritméticos/Relacionais/Lógicos)
+
+let falsa1 = 8 - 4 > 12 || 15 === "15";
+let falsa2 = !(7 !== 7 || "3" !== 3);
+
+console.log(falsa1);
+console.log(falsa2);
