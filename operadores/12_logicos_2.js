0d4c07eacfbfab89aa4b2dfa873eeff56f56e267
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-18 21:21:45 -0300

    Operadores Lógicos prática

---
+++ operadores/12_logicos_2.js
@@ -0,0 +1,14 @@
+let t1 = true;
+let t2 = true;
+
+let = comprarTV50 = t1 && t2; //And
+console.log("Vamos comprar a TV de 50\"?", comprarTV50);
+
+let comprarTV32 = t1 !== t2; //Xor
+console.log("Vamos comprar a TV de 32\"?", comprarTV32);
+
+let tomarSorvete = t1 || t2; //OR
+console.log("Vamos tomar sorvete?", tomarSorvete);
+
+let ficarEmCasa = !tomarSorvete; //NOT
+console.log("Vamos tomar sorvete?", ficarEmCasa);
