6059269d047915d0c12a792ab0b6fb5645cce667
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-21 16:56:27 -0300

    Desafio If

---
+++ 3_estrutura_de_controle.js/4_if_4.js
@@ -0,0 +1,10 @@
+// Ache o erro:
+
+const nota = 10;
+
+if (nota >= 9); {
+        console.log("Quadro de honra!");
+        console.log("Parabéns!");
+};
+
+// ponto e virgula depois da estrutura de controle.
