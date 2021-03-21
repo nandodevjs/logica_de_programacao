5051af029aa3d9a4772559ca04c1f0bb542cd405
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-21 19:19:20 -0300

    If && Else

---
+++ 3_estrutura_de_controle.js/6_if_else.js
@@ -0,0 +1,15 @@
+const verdadeiro = true;
+const falso = false;
+
+if (true){
+    console.log("Execultar 1");
+}
+else
+    console.log("Não execultar.");
+
+if (false)
+    console.log("Não execultar.");
+
+else {
+    console.log("Execultar 2");
+}
