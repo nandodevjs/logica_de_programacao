cf019c2d5d21638d0fb137c1e34b44724ebf649f
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-03-21 15:55:05 -0300

    Create 2_if_2.js

---
+++ 3_estrutura_de_controle.js/2_if_2.js
@@ -0,0 +1,10 @@
+const nota = 7.1;
+const bomComportamento = true;
+
+if (nota >= 7 && bomComportamento)
+    console.log("Pararabéns! Você foi aprovado");
+
+if (nota < 7 || !bomComportamento)
+    console.log("Infelizmente, você foi reprovado");
+
+console.log("Fim!");
