1bfc5e6bd027e70fb112103fb4105fee11a1c792
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-04-05 23:27:50 -0300

    Update 2_com_funcao.js

---
+++ 4_funcoes/2_com_funcao.js
@@ -0,0 +1,18 @@
+const d1 = 01;
+const m1 = 01;
+const a1 = 2001;
+
+function exibirData1 () {
+    console.log(`${d1}/${m1}/${a1}`);
+}
+
+const d2 = 02;
+const m2 = 02;
+const a2 = 2002;
+
+function exibirData2 () {
+    console.log(`${d2}/${m2}/${a2}`);
+}
+
+exibirData1();
+exibirData2();
