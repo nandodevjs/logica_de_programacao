04536edcf9e9a51be45f795dc4ad111c8a90b8a6
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-04-06 00:46:11 -0300

    3_com_funcao_2.js

---
+++ 4_funcoes/3_com_funcao_2.js
@@ -0,0 +1,14 @@
+const d1 = 01;
+const m1 = 01;
+const a1 = 2001;
+
+const d2 = 02;
+const m2 = 02;
+const a2 = 2002;
+
+function exibirData (d, m, a) {
+    console.log(`${d}/${m}/${a}`);
+}
+
+exibirData(d1, m1, a1);
+exibirData(d2, m2, a2);
