27f7a7695fac741a7f524e93eb10619e68a59af0
fernandodevjs <68031772+nandodevjs@users.noreply.github.com>
2021-04-05 23:15:46 -0300

    1_sem_funcao.js

---
+++ 4_funcoes/1_sem_funcao.js
@@ -0,0 +1,15 @@
+const d1 = 01;
+const m1 = 01;
+const a1 = 2001;
+
+console.log(`Dia ${d1}`);
+console.log(`Mês ${m1}`);
+console.log(`Ano ${a1}`);
+
+const d2 = 02;
+const m2 = 02;
+const a2 = 2002;
+
+console.log(`Dia ${d2}`);
+console.log(`Mês ${m2}`);
+console.log(`Ano ${a2}`);
